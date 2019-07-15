c3751815902ce1cf207582633555d49d02a336b6
asheem <amainaly16@gmail.com>
2019-07-14 23:29:58 -0400

    new assign

---
+++ springcame.demo/src/main/java/springcame/demo/SimpleRouteBuilder.java
@@ -0,0 +1,13 @@
+package springcame.demo;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.RoutesBuilder;
+
+public class SimpleRouteBuilder implements RoutesBuilder {
+
+	public void addRoutesToCamelContext(CamelContext arg0) throws Exception {
+		
+	}
+
+	
+}
