dd118b579713df8058a2f47ed5c7ff5c11551c76
asheem <amainaly16@gmail.com>
2019-07-14 22:51:30 -0400

    new assign

---
+++ Dynamic/DynamicF.java
@@ -0,0 +1,24 @@
+class DynamicF{
+  static int fibo (int n){
+int f[] = new int [n+2];
+int i;
+
+f[0] = 0;
+f[1] = 1;
+
+
+for(i=2; i<=n; i++){
+    f[i] = f[i-1]+f[i-2];
+}
+return f[n];
+
+}
+
+public static void main(String[] args){
+    int n = 9;
+    System.out.println(fibo(n));
+}
+
+
+
+  }
